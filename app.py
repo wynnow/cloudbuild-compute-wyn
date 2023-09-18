00e4315bdc316231f8b7084270e52594c30ecef3
Shahil Nissam <shahilnizzam@gmail.com>
2023-09-18 15:49:58 +0530

    welcome message changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)   #app instance
 #root of application 
 @app.route("/")       #app as a decorator to create each router or URL provided by application 
 def hello(): 
-    return "Hello, Welcome to Searce pvt ltd" 
+    return "Hello, Welcome to WYN cloud build test"
 
  
 if __name__ == "__main__": 
